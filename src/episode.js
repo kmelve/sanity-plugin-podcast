7fa42ec3b58fc94ef5d692c713d69e5393a7f2f9
Knut Melvær <knut.melvaer@gmail.com>
2018-08-07 12:31:43 +0200

    improve descriptions and preview of episodes

--- src/episode.js
+++ src/episode.js
@@ -1,8 +1,22 @@
+import SoundPlayerInput from './components/SoundPlayerInput/SoundPlayerInput'
 export default {
   name: 'episode',
   title: 'Episode',
   type: 'document',
   fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      required: true,
+      description: 'Remember that long titles can be truncated in podcast apps',
+      type: 'string'
+    },
+    {
+      name: 'podcast',
+      description: 'Choose podcast(s) to publish this episode in',
+      type: 'array',
+      of: [{ type: 'reference', weak: true, to: [{ type: 'podcast' }] }]
+    },
     {
       name: 'schedule',
       type: 'object',
@@ -46,18 +60,26 @@ export default {
       ]
     },
     {
-      name: 'podcast',
-      description: 'Choose podcast(s) to publish this episode in',
-      type: 'array',
-      of: [{ type: 'reference', weak: true, to: [{ type: 'podcast' }] }]
-    },
-    {
       name: 'file',
       title: 'Podcast media file',
-      required: true,
+      readOnly: true,
       description:
         'Most podcatchers support .mp3, but other audio-formats may work as well',
       type: 'file'
+    },
+    {
+      name: 'previewFile',
+      title: 'Podcast media file',
+      inputComponent: SoundPlayerInput,
+      description:
+        'Most podcatchers support .mp3, but other audio-formats may work as well',
+      type: 'string',
+    },
+    {
+      name: 'fileUrl',
+      title: 'External location for podcast media file',
+      description: 'For when you host your podcast media file elsewhere',
+      type: 'url'
     },
     {
       name: 'duration',
@@ -66,11 +88,10 @@ export default {
       type: 'string'
     },
     {
-      name: 'title',
-      title: 'Title',
-      required: true,
-      description: 'Remember that long titles can be truncated in podcast apps',
-      type: 'string'
+      name: 'subtitle',
+      type: 'string',
+      title: 'Subtitle',
+
     },
     {
       name: 'explicit',
@@ -80,16 +101,28 @@ export default {
     {
       name: 'summary',
       title: 'Summary',
-      description: 'iTunes ',
+      description: 'An episode summary is a string containing one or more descriptive sentences summarizing your episode for potential listeners. You can specify up to 4000 characters.',
       type: 'text'
     },
     {
       name: 'description',
       title: 'Description',
-      type: 'text'
+      description: `An episode description is a string containing one or more sentences describing your episode to potential listeners. You can specify up to 4000 characters.`,
+      type: 'text',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      description: 'An episode note. Where encoded is a string containing information about your episode.',
+      type: 'array',
+      of: [{
+        type: 'block'
+      }]
     },
     {
       name: 'linkList',
+      title: 'Link list',
+      description: 'A more structured way to add links for show notes. Will be compiled at the end of the episode content field in a podcast RSS feed',
       type: 'array',
       of: [
         {
@@ -106,6 +139,10 @@ export default {
             {
               name: 'excerpt',
               type: 'text'
+            },
+            {
+              name: 'timestamp',
+              type: 'string',
             }
           ]
         }
@@ -129,6 +166,9 @@ export default {
       name: 'tags',
       title: 'Tags',
       type: 'array',
+      options: {
+        layout: 'tags'
+      },
       of: [
         {
           type: 'string'
@@ -163,6 +203,67 @@ export default {
       name: 'coverArt',
       title: 'Cover art',
       type: 'image'
+    },
+    {
+      name: 'sponsors',
+      type: 'array',
+      title: 'Sponsors',
+      of: [
+        {
+          type: 'object',
+          title: 'Sponsor read',
+          fields: [
+            {
+              name: 'sponsor',
+              type: 'reference',
+              to: [
+                {
+                  type: 'sponsor'
+                }
+              ]
+            },
+            {
+              name: 'copy',
+              type: 'array',
+              title: 'copy',
+              of: [{ type: 'block'}]
+            },
+          ]
+        },
+      ]
+    },
+  ],
+  orderings: [
+    {
+      title: 'Publish Date, New',
+      name: 'publishDateDesc',
+      by: [
+        {field: 'schedule.publish', direction: 'desc'}
+      ]
+    },
+    {
+      title: 'Publish Date, Old',
+      name: 'publishDateAsc',
+      by: [
+        {field: 'schedule.publish', direction: 'asc'}
+      ]
     }
-  ]
+  ],
+  preview: {
+    select: {
+      title: 'title',
+      subtitle: 'podcast.0.title',
+      description: 'summary',
+      media: 'coverArt',
+      schedule: 'schedule'
+    },
+    prepare({title, subtitle, description, media, schedule}) {
+      return {
+        title,
+        esubtitle: `${new Date(schedule.publish).toDateString()} – ${subtitle}`,
+        description,
+        media,
+      }
+    }
+  }
 };
